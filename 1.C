e98eb7ccb8acad95095b9fa1c2ce4bb8dbfdcf37
ruhi-radadiya <ruhiradadiya78@gmail.com>
2023-11-06 18:41:26 +0530

    ass-1.2

---
+++ 1.C
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+printf("Name:Radadiya Ruhi Vipulbhai\nAge:17\nSchool:J.B and KARP\nCourse:flutter developer\nGRID:5956\nLocality:Surat");
+getch();
+}
